25ee1c69c0e862209f5b265d99db17ce2e24fb08
Aashana <aashanadhameja@gmail.com>
2018-07-01 16:15:43 +0530

    added card view and bottom sheet

--- app/src/main/java/com/example/joker/cosmos_android/Acitvity/SubAlgoActivity.java
+++ app/src/main/java/com/example/joker/cosmos_android/Acitvity/SubAlgoActivity.java
@@ -4,30 +4,41 @@ import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.example.joker.cosmos_android.Adapter.SubAlgoAdapter;
 import com.example.joker.cosmos_android.Database.SubAlgorithms;
 import com.example.joker.cosmos_android.R;
+import com.example.joker.cosmos_android.Utils.SubAlgorithmClickListener;
 import com.example.joker.cosmos_android.ViewModel.SubAlgoViewModel;
 
 import java.util.List;
 
-public class SubAlgoActivity extends AppCompatActivity {
+public class SubAlgoActivity extends AppCompatActivity implements SubAlgorithmClickListener{
 
     private SubAlgoViewModel viewModel;
 
-    private RecyclerView recyclerView;
+    private RecyclerView recyclerView,frecyclerView;
     private SubAlgoAdapter adapter;
+
+    private BottomSheetBehavior mBottomSheetBehavior;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub_algo);
+
+        android.view.View bottomSheet = findViewById(R.id.bottom_sheet);
+
+       mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+
 
         viewModel = ViewModelProviders.of(this).get(SubAlgoViewModel.class);
 
@@ -38,7 +49,14 @@ public class SubAlgoActivity extends AppCompatActivity {
         }
 
         recyclerView = findViewById(R.id.subAlgoRecyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+       // recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+
+        GridLayoutManager manager=new GridLayoutManager(this,2);
+        manager.setSpanSizeLookup(new GridLayoutManager.DefaultSpanSizeLookup());
+        recyclerView.setLayoutManager(manager);
+
 
         adapter = new SubAlgoAdapter(this);
         recyclerView.setAdapter(adapter);
@@ -55,5 +73,20 @@ public class SubAlgoActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+
+    @Override
+    public void languages(int id) {
+
+        Toast.makeText(this,"heyy card"+id,Toast.LENGTH_SHORT).show();
+        if(mBottomSheetBehavior.getState()==BottomSheetBehavior.STATE_COLLAPSED) {
+
+            mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+        }
+        else
+        {
+            mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        }
     }
 }
--- app/src/main/java/com/example/joker/cosmos_android/Adapter/SubAlgoAdapter.java
+++ app/src/main/java/com/example/joker/cosmos_android/Adapter/SubAlgoAdapter.java
@@ -1,32 +1,49 @@
 package com.example.joker.cosmos_android.Adapter;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.joker.cosmos_android.Acitvity.SubAlgoActivity;
 import com.example.joker.cosmos_android.Database.SubAlgorithms;
 import com.example.joker.cosmos_android.R;
+import com.example.joker.cosmos_android.Utils.SubAlgorithmClickListener;
 
 import java.util.List;
 
 public class SubAlgoAdapter extends RecyclerView.Adapter<SubAlgoAdapter.ViewHolder> {
 
+
     private Context context;
     private List<SubAlgorithms> subAlgorithms;
+    private SubAlgorithmClickListener clickListener;
+
+    public SubAlgoAdapter(SubAlgoActivity context) {
+        clickListener = context;
+    }
+
+
 
     public SubAlgoAdapter(Context context) {
         this.context = context;
     }
 
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.sub_algo_list_item,parent,false);
+      View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_row,parent,false);
+
         return new ViewHolder(v);
     }
 
@@ -35,6 +52,11 @@ public class SubAlgoAdapter extends RecyclerView.Adapter<SubAlgoAdapter.ViewHold
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
         holder.textView.setText(subAlgorithms.get(position).getSubAlgorithm());
+        holder.cardView.setOnClickListener((v)->
+        {
+            int id=subAlgorithms.get(position).getId();
+            clickListener.languages(id);
+        });
 
     }
 
@@ -53,14 +75,20 @@ public class SubAlgoAdapter extends RecyclerView.Adapter<SubAlgoAdapter.ViewHold
     }
 
 
+
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView textView;
+        CardView cardView;
+
 
         public ViewHolder(View itemView) {
             super(itemView);
 
-            textView = itemView.findViewById(R.id.subAlgoTextView);
+            textView = itemView.findViewById(R.id.textView);
+            cardView=itemView.findViewById(R.id.card_view);
+
+
 
         }
     }
